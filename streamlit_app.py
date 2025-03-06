cbe64748be05ece3b2c10116253f8253a4a9b52e
Khamdan AF <khamdan@std.unissula.ac.id>
2025-03-06 08:51:16 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
 
 # Streamlit Title
 st.title("Simulasi Pengurangan Losses dan Analisis Tegangan")
